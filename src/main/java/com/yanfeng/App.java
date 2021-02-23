c92610ac8ac796242f21d2a018b1e66afc54884d
Yan-Shi-Hao <73628029+Yan-Shi-Hao@users.noreply.github.com>
2021-02-23 17:32:54 +0800

    Update App.java
    远程有了第二个分支，需要拉去到本地

--- src/main/java/com/yanfeng/App.java
+++ src/main/java/com/yanfeng/App.java
@@ -7,5 +7,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("第二次提交");
         System.out.println("创建了一个新的分支：第一分支");
+        System.out.println("远程有了第二个分支，需要拉去到本地");
     }
 }
